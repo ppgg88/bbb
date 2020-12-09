ec56434ccf47c606304800630625c32f23b45e70
paul Giroux <paul.giroux87@gmail.com>
2020-12-09 17:00:17 +0100

    mdp -> pass

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $nbr = 0;
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=bbb;charset=utf8', 'admin', '*****');
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=bbb;charset=utf8', 'admin', 'pass');
 $action = $bdd->query('SELECT * FROM signatures');
 while ($val = $action->fetch()){
     $nbr++;
--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php 
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=bbb;charset=utf8', 'admin', '*******');
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=bbb;charset=utf8', 'admin', 'pass');
 if(!empty($_POST['nom']) AND !empty($_POST['num'])){
 
     $nom = htmlspecialchars($_POST['nom']);
